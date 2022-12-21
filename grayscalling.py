59ecc8925a4ea352af57ad93941cc6a236025824
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-12-22 05:06:06 +0530

    Update grayscalling.py

--- grayscalling.py
+++ grayscalling.py
@@ -1,10 +1,11 @@
 import cv2
-from google.colab.patches import cv2_imshow
-img = cv2.imread("input.jpg")
-cv2_imshow(img)
 
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Load the color image
+image = cv2.imread("input.jpg")
 
-cv2_imshow( gray_image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Display the grayscale image
+cv2_imshow(gray_image)
+cv2.waitKey(0)
