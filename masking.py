0fb0d231da86478975b3e2e692caf3788a020279
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-12-25 19:54:46 +0530

    Update masking.py

--- masking.py
+++ masking.py
@@ -1,11 +1,6 @@
 import cv2
 import numpy as np
 
-# If you're wondering why only two dimensions, well this is a grayscale image, 
-# if we doing a colored image, we'd use 
-# rectangle = np.zeros((300, 300, 3),np.uint8)
-
-# Making a sqare
 square = np.zeros((300, 300), np.uint8)
 cv2.rectangle(square, (50, 50), (250, 250), 255, -2)
 cv2.imshow("Square", square)
